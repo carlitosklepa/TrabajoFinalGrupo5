2dd21dc1e633de44e3a258a84d68855cf0152d82
carlitosklepa <85026273+carlitosklepa@users.noreply.github.com>
2021-12-13 11:54:23 -0300

    creación de apps

---
+++ tf_blog/apps/categorias/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Categoria(models.Model):
+    atributo_str = models.CharField(max_length=255)
+    atributo_decimal = models.DecimalField(max_digits=9, decimal_places=2)
--- tf_blog/tf_blog/settings.py
+++ tf_blog/tf_blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.categorias',
+    'apps.comentarios',
+    'apps.publicaciones',
+    'apps.usuarios'
 ]
 
 MIDDLEWARE = [
