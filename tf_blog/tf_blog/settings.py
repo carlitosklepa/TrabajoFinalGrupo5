cd456ce3d98ca69dea28ba4081e1fd6f4d2d39ae
carlitosklepa <85026273+carlitosklepa@users.noreply.github.com>
2021-12-18 17:13:43 -0300

    sin modificaciones

--- tf_blog/tf_blog/settings.py
+++ tf_blog/tf_blog/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "PORT": "5432"
     }
 }
-
+'''
 
 DATABASES = {
     'default': {
@@ -118,7 +118,7 @@ DATABASES = {
         "PORT": "5433"
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
