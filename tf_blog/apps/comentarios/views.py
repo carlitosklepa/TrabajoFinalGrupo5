64b8bf8ebb805a74f8d02a8f05df7580777bb1c4
carlitosklepa <85026273+carlitosklepa@users.noreply.github.com>
2021-12-20 21:37:04 -0300

    se agregaron mixins

--- tf_blog/apps/comentarios/views.py
+++ tf_blog/apps/comentarios/views.py
@@ -5,9 +5,10 @@ from .models import Comentario
 from apps.usuarios.models import Usuario
 from apps.publicaciones.models import Publicacion
 from .forms  import Comentario_Form
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Nuevo_Comentario(CreateView):
+class Nuevo_Comentario(LoginRequiredMixin, CreateView):
 	template_name = "comentarios/nuevo_comentario.html"
 	model = Comentario
 	form_class = Comentario_Form
--- tf_blog/apps/core/mixins.py
+++ tf_blog/apps/core/mixins.py
@@ -1,7 +1,15 @@
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
 
 class AdminRequiredMixins():
     def dispatch(self, request, *args, **kwars):
         if not request.user.es_administrador:
             raise PermissionDenied
         return super(AdminRequiredMixins, self).dispatch(request, *args, **kwars)
+
+
+class PermisosMixins(object):
+    def dispatch(self, request, *args, **kwars):
+        if request.user.es_escritor:
+            return super().dispatch(request, *args, **kwars)
+        return redirect("ingresar")
